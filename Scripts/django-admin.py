823bfec2a7445a48a4a047fe60cdcbccaf724d06
VidhikaGithub <31914040+VidhikaGithub@users.noreply.github.com>
2021-09-10 13:08:48 +0530

    Add files via upload

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\mss2015\desktop\final_project\myenvironment\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
